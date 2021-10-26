eaa8a3bd0a60a1ff1dda032e4720fe42aa901957
kubersharma001 <kubersharma001@gmail.com>
2021-10-26 13:38:06 +0000

    config: Disable blur on App Launch
    Ref: https://android.googlesource.com/platform/packages/apps/Launcher3/+/refs/tags/android-12.0.0_r3/quickstep/src/com/android/launcher3/statehandlers/DepthController.java#232
    
    Change-Id: Iab609dc9be5fbb8ccc5e124420a214e261fdc931

--- config/common.mk
+++ config/common.mk
@@ -120,6 +120,10 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.com.google.ime.kb_pad_port_b=0 \
     ro.com.google.ime.kb_pad_land_b=0
 
+# Blur
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.launcher.blur.appLaunch=false
+
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/batik/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/batik/overlay/common
 
