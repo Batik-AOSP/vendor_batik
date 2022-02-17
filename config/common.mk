e532f4f7a82121001d89bbba9af31f0b9179df43
Vishalcj17 <vishalcj17@gmail.com>
2022-02-17 11:17:47 +0100

    config: Enable blur by default
    Change-Id: Ie8403cee65e1157d6b2aea3d62b500ccadd7107f
    [nurkeinneid] make it conditional and disable by default
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>
    Signed-off-by: Eureka Fifthy <bangmeyseries@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -100,6 +100,16 @@ TARGET_SCREEN_HEIGHT ?= 1920
 PRODUCT_PACKAGES += \
     bootanimation.zip
 
+# Blur properties
+TARGET_USES_BLUR ?= false
+ifeq ($(TARGET_USES_BLUR), true)
+PRODUCT_PACKAGES += \
+    AndroidSystemBlur
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1 \
+    ro.surface_flinger.supports_background_blur=1
+endif
+
 PRODUCT_COPY_FILES += \
     vendor/batik/prebuilt/common/etc/init/init.batik-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.batik-updater.rc
 
