DerpFest-AOSP/vendor_derp

soong: Add camera_needs_client_info_lib

config/BoardConfigSoong.mk
@@ -30,6 +30,7 @@ SOONG_CONFIG_NAMESPACES += derpGlobalVars
 SOONG_CONFIG_derpGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    camera_needs_client_info_lib \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     has_memfd_backport \
@@ -64,6 +65,7 @@ SOONG_CONFIG_derpQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_derpGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_derpGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_derpGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_derpGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)

Batik-AOSP/vendor_batik

config/BoardConfigSoong.mk
@@ -30,6 +30,7 @@ SOONG_CONFIG_NAMESPACES += batikGlobalVars
 SOONG_CONFIG_batikGlobalVars += \
     additional_gralloc_10_usage_bits \
     bootloader_message_offset \
+    camera_needs_client_info_lib \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     has_memfd_backport \
@@ -64,6 +65,7 @@ SOONG_CONFIG_batikQcomVars += \
 endif
 
 # Soong bool variables
+SOONG_CONFIG_batikGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_batikGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_batikGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_batikGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)